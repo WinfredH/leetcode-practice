400548926cf5e7a09aca49e7fc3b37784e7fd186
WinfredH <hz873001860@live.cn>
2020-06-21 23:58:36 +0800

    leetcode-141: 环形链表

---
+++ leetcode-141/flag.js
@@ -0,0 +1,11 @@
+module.exports = function hasCycle(head) {
+  while (head) {
+    if (head.flag) {
+      return true
+    } else {
+      head.flag = true
+      head = head.next
+    }
+  }
+  return false
+}
